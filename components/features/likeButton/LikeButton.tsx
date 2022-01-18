35784a44a5211cb9355927bc73eed7e9e5defa71
phucthang1101 <phucthangvt1101@gmail.com>
2022-01-18 00:35:12 -0500

    update responsive css, add 2 pages and change some animation

--- components/features/likeButton/LikeButton.tsx
+++ components/features/likeButton/LikeButton.tsx
@@ -1,49 +1,122 @@
-import React from 'react';
-import { useState } from 'react';
-import { SpaceCard } from '../../models/SpaceCard';
-
+import React from 'react'
 import styles from './LikeButton.module.css';
 
-interface Props {
-    spaceCard: SpaceCard;
-    buttonClassName: string;
-    buttonClassNameActive: string;
-}
-const LikeButton = ({ spaceCard, buttonClassName, buttonClassNameActive }: Props) => {
-    const [likeBtnActive, setLikeBtnActive] = useState(localStorage.getItem(spaceCard.title) || "");
-
-
-    const handleLikeBtnClicked = () => {
-
-        if (likeBtnActive) {
-            //console.log('object');
-            localStorage.removeItem(spaceCard.title);
-            setLikeBtnActive("");
-        } else {
-            if (buttonClassName.includes("like")) {
-                localStorage.setItem(spaceCard.title, 'liked');
-                setLikeBtnActive("liked");
-            }
-            else if (buttonClassName.includes("clap")) {
-                localStorage.setItem(spaceCard.title, 'claped');
-                setLikeBtnActive("claped");
-            }
-
-        }
-    }
+const LikeButton = () => {
     return (
         <>
-            <span
-                className={`${buttonClassName}
-                 ${likeBtnActive && buttonClassName.includes(likeBtnActive) ?
-                        buttonClassNameActive : ""}`}
-                onClick={() => handleLikeBtnClicked()}
-            >
-
-            </span>
-
+            <input type="checkbox" className={styles.like_btn}/>
+            <i className={`fa fa-heart ${styles.fa_heart}`}></i>
         </>
-    );
-};
+    )
+}
 
-export default LikeButton;
+export default LikeButton
+
+
+
+// import React, { useRef, useEffect, useState, useCallback } from "react";
+// import styles from './LikeButton.module.css';
+
+// const LikeButton = () => {
+   
+    
+//     return (
+//        <>
+//         <input type="checkbox" className={styles.like_btn}>
+
+//             <i className="fa fa-heart"></i>
+//         </>
+//     );
+// }
+
+
+
+// export default LikeButton;
+    
+  
+    
+        
+        
+    
+
+
+
+    
+    
+    
+
+
+
+    
+        
+            
+            
+            
+                
+            
+        
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // import React from 'react';
+
+// // import { useState } from 'react';
+// // import { SpaceCard } from '../../models/SpaceCard';
+
+// // import styles from './LikeButton.module.css';
+
+// // interface Props {
+// //     spaceCard: SpaceCard;
+// //     buttonClassName: string;
+// //     buttonClassNameActive: string;
+// // }
+// // const LikeButton = ({ spaceCard, buttonClassName, buttonClassNameActive }: Props) => {
+// //     const [likeBtnActive, setLikeBtnActive] = useState(localStorage.getItem(spaceCard.title) || "");
+
+
+// //     const handleLikeBtnClicked = () => {
+
+// //         if (likeBtnActive) {
+// //             //console.log('object');
+// //             localStorage.removeItem(spaceCard.title);
+// //             setLikeBtnActive("");
+// //         } else {
+// //             if (buttonClassName.includes("like")) {
+// //                 localStorage.setItem(spaceCard.title, 'liked');
+// //                 setLikeBtnActive("liked");
+// //             }
+// //             else if (buttonClassName.includes("clap")) {
+// //                 localStorage.setItem(spaceCard.title, 'claped');
+// //                 setLikeBtnActive("claped");
+// //             }
+
+// //         }
+// //     }
+// //     return (
+// //         <>
+// //             <span
+// //                 className={`${buttonClassName}
+// //                  ${likeBtnActive && buttonClassName.includes(likeBtnActive) ?
+// //                         buttonClassNameActive : ""}`}
+// //                 onClick={() => handleLikeBtnClicked()}
+// //             >
+
+// //             </span>
+
+// //         </>
+// //     );
+// // };
+
+// // export default LikeButton;
